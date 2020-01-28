721f791e4c7ee2fb7d2ac280d5886b4c64f77bfd
Chris Palmer <capalmer1013@gmail.com>
2020-01-28 00:24:58 -0500

    added argparsing and got things running in main.py

--- sketches/f.py
+++ sketches/f.py
@@ -1,8 +1,8 @@
 global current_func
 def f(x, y, t):
-    #return (x*sin(y)*sin(trandom*0.5)*0.5*(sin(random.uniform(0, 1)*(sin(t)+1 or 1))))
+    return (x*sin(y)*sin(t*0.5)*0.5*(sin(random.uniform(0, 1)*(sin(t)+1 or 1))))
     # return (x%7) + (y%5)   + t *16 + x**2 / (y or 1)
-    return ((t)+1)*0.01 * (x* 0.01)*(y+ 40) + ((x-80)*y*sin(t)*0.1) 
+    # return ((t)+1)*0.01 * (x* 0.01)*(y+ 40) + ((x-80)*y*sin(t)*0.1) 
     # return random.randint(0, 256)
     # return (100*(t*0.5 + x + y)+1) * 2 /(t or 1)
     # return sin(x*y*t*0.5)*1.2 if  x % 5 else t*2+x if y % 7 else t+y
