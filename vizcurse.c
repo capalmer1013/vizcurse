48c1fe9c6c90f892d4fb3bb722e9e92ff5a659de
Chris Palmer <capalmer1013@gmail.com>
2023-04-01 02:57:39 -0400

    added forground and background, messed with the function until it looked cool

--- vizcurse.c
+++ vizcurse.c
@@ -1,3 +1,4 @@
+#include <sys/ioctl.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <math.h>
@@ -7,43 +8,48 @@
 #    define M_PI 3.14159265358979323846
 #endif
 
-int scaled_sin(int x) {
+int scaled_sin(int x, int scale) {
     double radians = x * (M_PI / 180.0); // Convert the integer to radians
     double sine_value = sin(radians); // Calculate the sine of the angle in radians
 
-    // Scale the sine value to be between 0 and 256, and then cast it to an int
-    int scaled_value = (int)((sine_value + 1) * 128);
+    // Scale the sine value to be between 0 and scale, and then cast it to an int
+    int scaled_value = (int)((sine_value + 1) * scale);
 
     return scaled_value;
 }
 
 void set_color(int x, int y, int t) {
     // Set the foreground color using ANSI escape codes with 24-bit RGB format
+    char* background = "\033[48;2;%d;%d;%dm";
+    char* foreground = "\033[38;2;%d;%d;%dm";
+    t *= 4;
     printf(
-        "\033[38;2;%d;%d;%dm",
-        scaled_sin(x)  % 256,
-        scaled_sin(y)  % 256,
-        scaled_sin(t)  % 256
+        background,
+        abs(y-scaled_sin(t, 256)                            ) % 256,
+        abs((x-scaled_sin(t, 256)) * y                  ) % 256,
+        abs(x * y + t                                     ) % 256
     );
 }
 
 int main() {
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     int x, y, t = 0;
-    int width   = 200;   // Console width
-    int height  = 50;    // Console height
+    int width   = w.ws_col;
+    int height  = w.ws_row;
 
     while (1) {
         for (y = 0; y < height; y++) {
             for (x = 0; x < width; x++) {
                 set_color(x, y, t);
-                putchar('#');
+                putchar(' ');
             }
             putchar('\n');
         }
         fflush(stdout);
 
         t++;            // Increment time (count of complete for loop iterations)
-        usleep(10000);  // Add some delay (in microseconds) between frames
+        usleep(33333); // Sleep for 1/30 second
 
         // Reset the console cursor position to the top-left corner
         printf("\033[%dA", height);
